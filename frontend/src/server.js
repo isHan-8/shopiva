3f6ceee71c699fcd0177a098d9bea77c4411e7b4
isHan-8 <0808cs211078.ies@ipsacademy.org>
2024-07-07 18:35:05 +0530

    Shopiva

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,15 @@
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
 
-export  const  backend_url = "http://localhost:8000";
+// export  const  backend_url = "http://localhost:8000";
+
+
+
+
+
+
+
+export const server = "https://shopiva.vercel.app/test/api/v2";
+
+
+export  const  backend_url = "https://shopiva.vercel.app/test";
